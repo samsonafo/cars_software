5c9a556a0f089f9d21c4763a28f919d6c361b7c4
Samson Afolabi <afolabisamson15@gmail.com>
2021-01-21 08:39:51 +0100

    updates 21.01.2021

--- app.py
+++ app.py
@@ -27,6 +27,33 @@ app = Flask(__name__, template_folder='templates',
 def home():
     return render_template("index.html")
 
+@app.route('/result',  methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        # get data and convert data into dataframe
+        Millage = request.form['Millage']
+        History = request.form['History']
+        Model_Year = request.form['Model-Year']
+        Transmission = request.form['Transmission']
+        Make = request.form['Make']
+        Model = request.form['Model']
+        Ratings = request.form['Ratings']
+        City = request.form['City']
+         
+        year = time.strftime("%Y")
+        Age = year - Model_Year
+        
+        
+        
+        data_df = pd.DataFrame([[Millage,History,Age,Transmission,Make,Model,Ratings,City]],
+                               columns=['Millage','History','Age','Transmission','Make','Model','Ratings','City'])
+        # predictions
+        output = model.predict(data_df)
+        #result = np.int(np.round(output))
+        
+    # return data
+    return render_template("index.html#result",prediction=output)
+
 
 if __name__ == '__home__':
     app.run(port = 5000, debug=True)
